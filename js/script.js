b5d60b9d9db9f8a0bf3f148fe4c5569981b0f585
AhmedM1643 <ahmedabdelsamiea07@gmail.com>
2023-08-26 07:20:14 +0200

    saved the old  version of the repository

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import {} from "./swiper.js";
+
+const swiper = new Swiper();
+// const Scroll = ScrollReveal({
+//   origin: 'top',
+//   distance: '60px',
+//   duration: 1500,
+//   delay: 200,
+// //     reset: true
+// });
+
+// Scroll.reveal('.Main,.section-title,.AboutMe,.Skills,.Skill,.Exp,.Services,.GridLayout,.Service,.Projects,.Project,.Contact,.ContactForm,Input,textarea',{}); 
